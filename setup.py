5c2617a0fe034d25595e02a8011cb8de74b87f2a
Matthias Mattanovich <57752712+mmattano@users.noreply.github.com>
2022-06-01 09:29:54 +0200

    feat: added standard files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="transnet",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Database connection and trans-omics network construction",
+    author="Matthias Mattanovich",
+    license="MIT",
+)
