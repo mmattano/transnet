4a2385f2b8f212e28eb4bb4ebdbde75b6f06f81f
mmattano <matthias.mattanovich@gmail.com>
2025-05-15 11:06:09 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,42 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="transnet",
-    packages=find_packages(),
     version="0.1.0",
-    description="Database connection and trans-omics network construction",
-    author="Matthias Mattanovich",
-    license="MIT",
+    author="Matthias Anagho-Mattanovich",
+    author_email="matthias.mattanovich@sund.ku.dk",
+    description="A package for trans-omics data integration and network analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mmattano/transnet",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "networkx",
+        "matplotlib",
+        "scipy",
+        "bioservices",
+        "mygene",
+        "python-louvain",  # community detection
+        "requests",
+        "biopython"
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "black",
+            "flake8"
+        ]
+    }
 )
